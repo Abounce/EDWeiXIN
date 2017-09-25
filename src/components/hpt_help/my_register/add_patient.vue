d63d70b0752d78f70ea7987d792b857b93f820d7
Abounce <784354821@qq.com>
2017-09-25 18:01:22 +0800

    v1.3.5添加就诊人

--- src/components/hpt_help/my_register/add_patient.vue
+++ src/components/hpt_help/my_register/add_patient.vue
@@ -1,11 +1,62 @@
 <template>
- <div>aa</div>
+ <div>
+     <div class="tab">
+       <div class="tab-man" @click="chooseman" :class="{selectleft:isman}">成人</div>
+       <div class="tab-child" @click="choosechild" :class="{selectleft:ischild}">儿童/新生儿</div>
+     </div>
+     <div v-show="isman" class="man"></div>
+     <div v-show="ischild" class="child"></div>
+ </div>
 </template>
 
 <script>
-
+  export default {
+    data(){
+      return{
+        isman:true,
+        ischild:false
+      }
+    },
+    methods:{
+      chooseman(){
+        this.isman=true
+        this.ischild=false
+      },
+      choosechild(){
+        this.isman=false
+        this.ischild=true
+      }
+    }
+  }
 </script>
 
 <style scoped lang="less" type="text/less">
+    .tab{
+      display: flex;
+      height: 49px;
+      .tab-man{
+        line-height: 49px;
+        text-align: center;
+        flex: 1;
+        background: red;
+      }
+      .tab-child{
+        line-height: 49px;
+        text-align: center;
+        flex: 1;
+        background: yellow;
+      }
+    }
+    .man{
+      margin-top: 10.5px;
+    }
+    .child{
+      margin-top: 10.5px;
+    }
+    .selectleft{
+      color: #13bf72;
+      /*border-bottom: 1px solid #13bf72;*/
+
+    }
 
 </style>
