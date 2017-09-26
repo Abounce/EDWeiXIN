a9d95d313c17d285f101537609297419af47aae8
Abounce <784354821@qq.com>
2017-09-26 13:53:18 +0800

    v1.3.7添加就诊布局完成

--- src/components/hpt_help/my_register/add_patient.vue
+++ src/components/hpt_help/my_register/add_patient.vue
@@ -35,22 +35,75 @@
          <label for="autoinput" class="one">验证码:</label>
          <input type="text" id="autoinput" v-model="automsg" class="two"  placeholder="请填写验证码">
          <div class="weui-icon-clear three" ></div>
-       </div>
-       <div class="visitcard" style="text-align: right">
-
-           <span>没有就诊卡?</span> <a href="javascript:">在线建卡</a>
+         <a href="javascript:">获取验证码</a>
        </div>
        <div class="iscard">
          <label for="iscardinput" class="one">就诊卡号:</label>
          <input type="text" id="iscardinput" v-model="iscardmsg" class="two"  placeholder="请填写就诊卡号" >
          <div class="weui-icon-clear three" ></div>
        </div>
+       <div class="visitcard" style="text-align: right">
+
+           <span>没有就诊卡?</span> <a href="javascript:">在线建卡</a>
+       </div>
        <div class="button">
          <span>添加</span>
        </div>
      </div>
-     <div v-show="ischild" class="child"></div>
-       <!--<check-icon :value.sync="demo1">Do you agree? ({{ demo1 }})</check-icon>-->
+     <div v-show="ischild" class="child">
+       <div class="name">
+         <label for="nameinputa" class="one" >真实姓名:</label>
+         <input type="text" id="nameinputa" v-model="namemsg" class="two" placeholder="请填写就诊人姓名" onkeyup="value=value.replace(/[^\u4E00-\u9FA5]/g,'')">
+         <div class="weui-icon-clear three" ></div>
+       </div>
+       <div class="sex">
+         <span class="one">性别:</span>
+         <check-icon :value.sync="demo1" class="two"   @click.native="nan">男</check-icon>
+         <check-icon :value.sync="demo2" class="three" @click.native="nv">女</check-icon>
+         <check-icon :value.sync="demo3" class="four"  @click.native="baomi">保密</check-icon>
+       </div>
+       <div class="birthday">
+         <label for="birthdayinputa" class="one">出身日期:</label>
+         <input type="text" id="birthdayinputa" v-model="birthdaymsg" class="two"  >
+         <div class="weui-icon-clear three" ></div>
+       </div>
+       <div class="card">
+         <label for="cardinputa" class="one"  >患者身份证:</label>
+         <input type="text" id="cardinputa" v-model="cardmsg" class="two"  placeholder="请填写就诊人身份证号">
+         <div class="weui-icon-clear three" ></div>
+       </div>
+       <div class="phone">
+         <label for="nameinputa" class="one">监护人手机:</label>
+         <input type="text" id="phoneinputa" v-model="phonemsg" class="two"  placeholder="请填写建卡预留手机号">
+         <div class="weui-icon-clear three" ></div>
+       </div>
+       <div class="autocode">
+         <label for="autoinputa" class="one">验证码:</label>
+         <input type="text" id="autoinputa" v-model="automsg" class="two"  placeholder="请填写验证码">
+         <div class="weui-icon-clear three" ></div>
+         <a href="javascript:">获取验证码</a>
+       </div>
+
+       <div class="jhcard">
+         <label for="jhcardinput" class="one">监护人身份证:</label>
+         <input type="text" id="jhcardinput" v-model="automsg" class="two"  placeholder="请填写监护人身份证">
+         <div class="weui-icon-clear three" ></div>
+       </div>
+       <div class="iscard">
+         <label for="iscardinputa" class="one">就诊卡号:</label>
+         <input type="text" id="iscardinputa" v-model="iscardmsg" class="two"  placeholder="请填写就诊卡号" >
+         <div class="weui-icon-clear three" ></div>
+       </div>
+
+       <div class="visitcard" style="text-align: right">
+
+         <span>没有就诊卡?</span> <a href="javascript:">在线建卡</a>
+       </div>
+       <div class="button">
+         <span>添加</span>
+       </div>
+     </div>
+
  </div>
 </template>
 
@@ -127,7 +180,7 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -177,7 +230,7 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -197,7 +250,7 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -217,7 +270,7 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -237,7 +290,205 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 28;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 40;
+        }
+        .three{
+          flex: 8;
+          line-height: 49px;
+          text-align: center;
+        }
+        a{
+          flex: 27;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .iscard{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 65;
+        }
+        .three{
+          flex: 10;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .visitcard{
+        margin-top: 15px;
+
+      }
+      .button{
+        margin-top: 30px;
+        text-align: center;
+        span{
+          color: white;
+          padding:2% 42%;
+          background-color: #13bf72;;
+          border-radius: 5px;
+          border: solid 1px #11ad67;
+        }
+      }
+    }
+    .child{
+      margin-top: 10.5px;
+      .name{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 65;
+        }
+        .three{
+          flex: 10;
+          line-height: 49px;
+          text-align: center;
+        }
+
+      }
+      .sex{
+        display: flex;
+        margin-top: 13px;
+        height: 36px;
+        .one{
+          flex: 1;
+          /*height: 49px;*/
+          /*line-height: 49;*/
+          text-align: right;
+        }
+        .two{
+
+          flex: 1;
+          /*line-height: 49;*/
+          text-align: center;
+        }
+        .three{
+          flex: 1;
+          /*line-height: 49;*/
+          text-align: center;
+          /*height: 49px;*/
+        }
+        .four{
+          /*height: 49px;*/
+          flex: 1;
+          /*line-height: 49;*/
+          text-align: center;
+        }
+      }
+      .birthday{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 65;
+        }
+        .three{
+          flex: 10;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .card{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 65;
+        }
+        .three{
+          flex: 10;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .phone{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 65;
+        }
+        .three{
+          flex: 10;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .autocode{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 28;
+          text-align: right;
+          line-height: 49px;
+        }
+        .two{
+          border: none;
+          outline: medium;
+          line-height: 49px;
+          flex: 40;
+        }
+        .three{
+          flex: 8;
+          line-height: 49px;
+          text-align: center;
+        }
+        a{
+          flex: 27;
+          line-height: 49px;
+          text-align: center;
+        }
+      }
+      .jhcard{
+        height: 49px;
+        display: flex;
+        .one{
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -257,7 +508,7 @@
         height: 49px;
         display: flex;
         .one{
-          flex: 25;
+          flex: 30;
           text-align: right;
           line-height: 49px;
         }
@@ -273,9 +524,21 @@
           text-align: center;
         }
       }
-    }
-    .child{
-      margin-top: 10.5px;
+      .visitcard{
+        margin-top: 15px;
+
+      }
+      .button{
+        margin-top: 30px;
+        text-align: center;
+        span{
+          color: white;
+          padding:2% 42%;
+          background-color: #13bf72;;
+          border-radius: 5px;
+          border: solid 1px #11ad67;
+        }
+      }
     }
     .selectleft{
       color: #13bf72;
