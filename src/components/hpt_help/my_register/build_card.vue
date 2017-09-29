7163e4552603ac170686d17271187a1d8a32b4ed
Abounce <784354821@qq.com>
2017-09-29 11:45:29 +0800

    v1.2.0

--- src/components/hpt_help/my_register/build_card.vue
+++ src/components/hpt_help/my_register/build_card.vue
@@ -34,11 +34,33 @@
        </div>
      </div>
      <div class="credentials">
-       <div></div>
-       <div></div>
+       <div>
+         <div></div>
+         <div>上传身份证正面</div>
+       </div>
+       <div>
+         <div></div>
+         <div>上传身份证反面</div>
+       </div>
      </div>
-     <div class="msg"></div>
-     <div class="button"></div>
+     <div class="msg">
+         <div class="warpper">
+           <div class="one"></div>&nbsp;成人用户上传身份证照片<span>（正反面)</span>;
+         </div>
+       <div class="warpper">
+         <div class="one"></div>&nbsp;<span>儿童</span>如果没有身份证的需上传<span>户口证户主页面和本人当页</span>；新生儿上传<span>母亲户口当页和本人出生证明</span>,身份证号码栏填写<span>母亲的身份证号</span>
+       </div>
+       <div class="warpper">
+         <div class="one"></div>&nbsp;请上传真实有效、清晰的证件照片，并填写真实的资料信息，以免造成诊疗信息的错误；
+       </div>
+       <div class="warpper">
+         <div class="one"></div>&nbsp;资料提交后需要医院人工审核，<span>未审核或审核未通过不能进行挂号</span>，请耐心等待。
+       </div>
+
+     </div>
+     <div class="button">
+          <span>确定</span>
+     </div>
    </div>
 </template>
 
@@ -161,4 +183,34 @@
        }
      }
    }
+
+  .msg{
+    .warpper{
+      margin-left: 15px;
+      margin-top: 20px;
+      font-size: 15px;
+      .one{
+        display: inline-block;
+        width: 9px;
+        height: 9px;
+        background-color: #cdcdcd;
+        border-radius: 50%;
+      }
+      span{
+        color: red;
+      }
+    }
+  }
+
+  .button{
+    margin-top:30px;
+    text-align: center;
+    span{
+      color: white;
+      padding:5% 42%;
+      background-color: #13bf72;;
+      border-radius: 5px;
+      border: solid 1px #11ad67;
+    }
+  }
 </style>
